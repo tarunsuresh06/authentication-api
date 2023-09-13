60fe7186179762e2d392f2c55e80e1f4c5ac2db0
tarunsuresh <tarunsuresh.v@gmail.com>
2023-09-13 23:33:56 +0530

    Add Register API Method

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 const app = express();
 
+const bcrypt = require('bcrypt')
+
 const dbPath = path.join(__dirname, "tarunsuresh.db");
 
 let db = null;
@@ -25,3 +27,34 @@ const initializeDBAndServer = async () => {
 };
 
 initializeDBAndServer();
+
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+
+  const checkUsernameQuery = `
+  SELECT username from users WHERE username= '${username}';
+  `;
+
+  const checkUser = await db.get(checkUsernameQuery);
+
+  if (checkUser === null) {
+    if (password.length < 6) {
+      res.status(400);
+      res.send("Password is too short");
+    } else {
+
+        const hashedPassword = await bcrypt.hash(password, 10); 
+        const createUserQuery = `
+          INSERT INTO users(username, password)
+          VALUES (${username}, ${hashedPassword});
+          `;
+
+        const dbResponse = await db.run(createUserQuery);
+        const userId = dbResponse.lastID;
+        res.send({ userId: userId });
+      }
+  } else {
+    res.status(400);
+    res.send("User already exists");
+  }
+});

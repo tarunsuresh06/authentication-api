3982e9b9ac8be32a8280dcb4d8325b2b5c09887b
Tarun suresh <88505951+tarunsuresh06@users.noreply.github.com>
2023-09-21 23:50:14 +0530

    Enable CORS for requests

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ const app = express();
 
 app.use(express.json());
 
+// Enable CORS for requests from http://localhost:3000
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 const bcrypt = require('bcrypt');
 
 const jwt = require('jsonwebtoken')
